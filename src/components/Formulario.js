fef4b0a86c73d170874b699c5bc3196ed7c61191
Jeral <jeral.cris@gmail.com>
2021-12-27 17:41:28 -0600

    leyendo la informacion del form y pasarla al state

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -1,9 +1,42 @@
+import { useState } from 'react';
+import Error from './Error';
+
 const Formulario = () => {
+   const [busqueda, setBusqueda] = useState({
+      artista: '',
+      cancion: '',
+   });
+   const [error, setError] = useState(false);
+
+   // Leer el contenido del formulario
+   const handleChange = (e) => {
+      setBusqueda({
+         ...busqueda,
+         [e.target.name]: e.target.value,
+      });
+   };
+   const { artista, cancion } = busqueda;
+
+   const handleSubmit = (e) => {
+      e.preventDefault();
+
+      // Validamos que los campos del formulario
+      if (artista.trim() === '' || cancion.trim() === '') {
+         setError(true);
+         return;
+      }
+      setError(false);
+   };
+
    return (
       <div className='bg-light'>
          <div className='container'>
             <div className='row'>
-               <form className='col card text-white bg-transparent mb-5 pt-5 pb-2'>
+               <form
+                  //
+                  className='col card text-white bg-transparent mb-5 pt-5 pb-2'
+                  onSubmit={handleSubmit}
+               >
                   <fieldset>
                      <legend className='text-center'>Buscar Letras de Canciones</legend>
                      <div className='row'>
@@ -16,6 +49,8 @@ const Formulario = () => {
                                  name='artista'
                                  className='form-control'
                                  placeholder='Nombre Artista'
+                                 onChange={handleChange}
+                                 value={artista}
                               />
                            </div>
                         </div>
@@ -29,11 +64,28 @@ const Formulario = () => {
                                  name='cancion'
                                  className='form-control'
                                  placeholder='Nombre Canción'
+                                 onChange={handleChange}
+                                 value={cancion}
                               />
                            </div>
                         </div>
                      </div>
-                     <button className='btn btn-secondary float-right'>Buscar</button>
+
+                     <button
+                        //
+                        type='submit'
+                        className='btn btn-secondary float-right'
+                     >
+                        Buscar
+                     </button>
+
+                     {/* Alerta si hay error */}
+                     {error ? (
+                        <Error
+                           //
+                           mensaje='Los campos son obligatorios'
+                        />
+                     ) : null}
                   </fieldset>
                </form>
             </div>
